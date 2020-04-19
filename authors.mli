f9420cba8c20ac55803b084f22a4bb363ab7c48f
Tony Yang <txy3@cornell.edu>
2020-04-19 19:20:41 -0400

    setup up authors.ml/authors.mli

---
+++ authors.mli
@@ -0,0 +1,8 @@
+(**
+   Metadata about this submission.
+   @author cjr269, jmg572, txy3
+*)
+
+(** [hours_worked] is a list of the number of hours each team member
+    worked on this assignment. *)
+val hours_worked : int list
