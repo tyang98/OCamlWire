268437d771b163574d90a28fe7fb0a6d2bba8571
Tony Yang <txy3@cornell.edu>
2020-04-19 22:57:53 -0400

    implement type t and size of board

--- board.ml
+++ board.ml
@@ -1,6 +1,14 @@
-type t = unit
-type tile = unit
-type bonus = unit
+
+type bonus = WordBonus of int | LetterBonus of int * char
+
+type tile = {
+  x_coord: int;
+  y_coord: int;
+  filled : char;
+  isBonus : bonus option;
+}
+
+type t = tile list
 
 let query_tile r c b = failwith "Unimplemented"
 
@@ -8,4 +16,5 @@ let check_bonus r c b = failwith "Unimplemented"
 
 let set_tile r c l b = failwith "Unimplemented"
 
-let size b = failwith "Unimplemented"
+let size b = 
+  List.length b
