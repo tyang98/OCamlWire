56aeab4e8817c352880d5e07821237ef7b12f8ee
Tony Yang <txy3@cornell.edu>
2020-04-19 21:33:37 -0400

    Update proposedMove.mli

--- proposedMove.mli
+++ proposedMove.mli
@@ -1,8 +1,8 @@
-(** The type of directions representing either across or down *)
-type direction = Across | Down
-
 (** [t] is the type representing a proposed move *)
 type t 
+
+(** The type of directions representing either across or down *)
+type direction = Across | Down
 
 (** [letters m] is the list of characters representing each letter used 
     for proposed move [m]. *)
