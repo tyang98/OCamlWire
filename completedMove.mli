650cfaeb117950565a5ab722c74d1649cef612bb
chrisco210 <chrisrachlinski@gmail.com>
2020-04-20 16:52:09 -0400

    Fix bug in completedMove where doubled letters would count for each letter in the word, add corresponding tests

--- completedMove.mli
+++ completedMove.mli
@@ -1,14 +1,31 @@
+(** [LetterValueMap] is a module type for modules which map letters to 
+    point values *)
 module type LetterValueMap = sig 
   val get : char -> int
 end 
 
+(** [CompletedMove] is a signature of a CompletedMove module created from Make 
+*)
 module type CompletedMove = sig
+  (** [t] is the type of a completed move*)
   type t
 
+  (** [LV] is the LetterValueMap we use to get the values of letters*)
   module LV : LetterValueMap
 
+  (** [score c] is the total score accomplished by this move *)
   val score : t -> int
+
+  (** [words c] is the list of words in this completed move*)
   val words : t -> string list
+
+  (** [from (word, (letter, mult), mult)] is a CompletedMove consisting of the 
+      concatenation of each [word], the letter bonuses given by [letter] * 
+      [mult], and the word bonuses given by [mult]
+  *)
+  val from : (string * (char * int) list * int list) list -> t
 end
 
+(** [Make LetterVal] creates a CompletedMove from a LetterValueMap.  The created
+    CompletedMove module will use LetterVal to get values of letters *)
 module Make : functor (LetterVal : LetterValueMap) -> CompletedMove
--- test.ml
+++ test.ml
@@ -1,9 +1,30 @@
 open OUnit2
 
-let tests = [
-  "Test test" >:: (fun _ -> assert_equal 1 0)
+module SCM = StandardCompletedMove.StandardCompletedMove
+
+
+let completed_move_scm_test (name : string) (cm : SCM.t ) (e : int) : test = 
+  name >:: (fun _ -> assert_equal e (SCM.score cm) ~printer:(string_of_int)) 
+
+let totNoBonus =  SCM.from [("tot", [], [])]
+let totDoubleLetter = SCM.from [("tot", [('t', 2)], [])]
+let totDoubleWord = SCM.from [("tot", [], [2])]
+
+let completed_move_tests = [
+  completed_move_scm_test "Simple 3 letter test no bonuses" totNoBonus 3;
+  completed_move_scm_test "Simple 3 letter double word" totDoubleWord 6;
+  completed_move_scm_test 
+    "only 1 bonus letter, but 2 letters in word" totDoubleLetter 4;
+  completed_move_scm_test 
+    "Simple 3 letter test with triple value 1 letter matching" 
+    (SCM.from [("got", [('g', 3)], [])]) 8;
+  completed_move_scm_test "Letter and word bonus"
+    (SCM.from [("got", [('g', 3)], [2])]) 16;
 ]
 
-let suite = "search test suite" >::: tests
+let tests = [
+]
+
+let suite = "search test suite" >::: List.flatten [tests; completed_move_tests]
 
 let _ = run_test_tt_main suite
