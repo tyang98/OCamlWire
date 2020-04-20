5e8278e53ea80050849f5fd3216fdd3666436ad4
Jack Greenberg <kcaj@grebneerg.com>
2020-04-19 21:23:18 -0400

    Added Dictionary interface and updated build system

---
+++ trieDictionary.ml
@@ -0,0 +1,16 @@
+open Dictionary
+open Map
+
+module Make = functor (K : OrderedType) -> struct
+  module Key = K
+
+  module Children = Map.Make(Key)
+
+  type 'a t = Node of 'a option * 'a t Children.t
+
+  let insert word e t = failwith "Unimplemented"
+
+  let get word t = failwith "Unimplemented"
+
+  let check word t = failwith "Unimplemented"
+end
