dcf2d0d86a44d2198af8f47dbf0e42d66f89eeb3
Jack Greenberg <kcaj@grebneerg.com>
2020-04-22 20:05:29 -0400

    Documentation

--- dictionary.mli
+++ dictionary.mli
@@ -1,13 +1,24 @@
+(** A dictionary for efficiently checking if keys exist, and optionally
+    retrieving associated values. *)
 module type Dictionary = sig
+  (** The type of an entry in the dictionary. *)
   type entry
 
+  (** The type of values associated with an [entry]. *)
   type 'a t
 
+  (** An empty dictionary. *)
   val empty : 'a t
 
+  (** [insert entry value dict] is [dict] updated to contain [entry] and
+      [value]. If [entry] already exists, [value] replaces its associated value.
+  *)
   val insert : entry -> 'a -> 'a t -> 'a t
 
+  (** [get entry dict] is an option containing the value associated with
+      [entry], or [None] if [entry] is not in [dict]. *)
   val get : entry -> 'a t -> 'a option
 
+  (** [check entry dict] is whether or not [dict] contains [entry]. *)
   val check : entry -> 'a t -> bool
 end
