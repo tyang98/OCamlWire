95e6d9d98f31f87329527ea48a0742656ad9c673
Tony Yang <txy3@cornell.edu>
2020-04-23 22:42:53 -0400

    finish intial board with all bonuses

--- main.ml
+++ main.ml
@@ -5,8 +5,9 @@ open Player
 open Gameplay
 open State
 
-let rec main () = 
+
+let main () = 
   ANSITerminal.(print_string [red; Bold] (" " ^
-                                          "\n\n Welcome to OScrabble, a functional implementaiton of Scrabble.\n"))
+                                          "\n\n Welcome to OScrabble, a functional implementation of Scrabble.\n"))
 
 let () = main ()
--- state.ml
+++ state.ml
@@ -18,28 +18,54 @@ let rec make_players playerc ps = match playerc with
 
 
 (* Board Config:
-   Double Letter Score:
-   (1,4)  (3,7) (4,1)  (7,3) (8,4)  (9,3) (12,1) (13,7) (15,4) 
-   (1,12) (3,9) (4,8)  (7,7) (8,12) (9,7) (12,8) (13,9) (15,12)
-                (4,15) (7,9)        (9,9) (12,15)
-                       (7,13)       (9,13)
    Double Word Score:
-   (2,2)  (3,3)  (4,4)  (5,5)  (11,5)  (12,4)  (13,3)  (14,2)
-   (2,14) (3,13) (4,12) (5,11) (11,11) (12,12) (13,13) (14,14) 
+   (1,1)  (2,2)  (3,3)  (4,4)  (10,4)  (11,3)  (12,2)  (13,1)
+   (1,13) (2,12) (3,11) (4,10) (10,10) (11,11) (12,12) (13,13) 
+   Triple Word Score:
+   (0,0)  (7,0)  (14,0)
+   (0,7)  (7,14) (14,7)
+   (0,14)        (14,14)
+
+    Double Letter Score:
+   (0,3)  (2,6) (3,0)  (6,2) (7,3)  (8,2) (11,0) (12,6) (14,3) 
+   (0,11) (2,8) (3,7)  (6,6) (7,11) (8,6) (11,7) (12,8) (14,11)
+                (3,14) (6,8)        (8,8) (11,14)
+                       (6,12)       (8,12)
 
    Triple Letter Score:
-   (2,6)   (6,2)  (10,2)  (14,6)
-   (2,10)  (6,6)  (10,6)  (14,10)
-          (6,10) (10,10)
-          (6,14) (10,14) 
-   Triple Word Score:
-   (1,1)  (8,1)  (15,1)
-   (1,8)  (8,15) (15,8)
-   (1,15)        (15,15)
+   (1,5)   (5,1)  (9,1)  (13,5)
+   (1,9)  (5,5)  (9,5)  (13,9)
+          (5,9) (9,9)
+          (5,13) (9,13) 
 *)
 
 let init_state player = 
-  let start = [] in
+  let start = [
+    (0,0, WordBonus 3); (0,3, LetterBonus (2,' ')); (0,7, WordBonus 3); 
+    (0,11, LetterBonus (2,' ')); (0,14, WordBonus 3); 
+    (1,1, WordBonus 2); (1,5, LetterBonus (3,' ')); (1,9, LetterBonus (3,' '));
+    (1,13, WordBonus 2); (2,2, WordBonus 2); 
+    (2,6, LetterBonus (2,' ')); (2,8, LetterBonus (2,' ')); (2,12, WordBonus 2);
+    (3,0, LetterBonus (2,' ')); (3,3, WordBonus 2); (3,7, LetterBonus (2,' ')); 
+    (3,13, WordBonus 2); (3,14, LetterBonus (2,' '));
+    (4,4, WordBonus 2); (4,10, WordBonus 2);
+    (5,1, LetterBonus (3,' ')); (5,5, LetterBonus (3,' '));
+    (5,9, LetterBonus (3,' ')); (5,13, LetterBonus (3,' ')); 
+    (6,2, LetterBonus (2,' ')); (6,6, LetterBonus (2,' ')); 
+    (6,8, LetterBonus (2,' ')); (6,12, LetterBonus (2,' '));
+    (7,0, WordBonus 3); (7,3, LetterBonus (2,' ')); (7,11, LetterBonus (2,' ')); 
+    (7,14, WordBonus 3); (8,2, LetterBonus (2,' ')); (8,6, LetterBonus (2,' '));
+    (8,8, LetterBonus (2,' ')); (8,12, LetterBonus (2,' ')); 
+    (9,1, LetterBonus (3,' ')); (9,5, LetterBonus (3,' '));
+    (9,9, LetterBonus (3,' ')); (9,13, LetterBonus (3,' '));
+    (10,4, WordBonus 2); (10,10, WordBonus 2); (11,0, LetterBonus (2,' '));
+    (11,3, WordBonus 2); (11,7, LetterBonus (2,' ')); (11,11, WordBonus 2); 
+    (11,14, LetterBonus (2,' ')); (12,2, WordBonus 2); 
+    (12,6, LetterBonus (2,' ')); (12,8, LetterBonus (2,' '));
+    (12,12, WordBonus 2); (13,1, WordBonus 2); (13,5, LetterBonus (3,' '));
+    (13,9, LetterBonus (3,' ')); (13,13, WordBonus 2);  
+    (14,0, WordBonus 3); (14,3, LetterBonus (2,' '));(14,7, WordBonus 3); 
+    (14,11, LetterBonus (2,' '));(14,14, WordBonus 3)] in
   {
     gameplay = make_gameplay (Board.init_board start 15) 
         (WordChecker.load_from_file "scrabble.txt"); 
